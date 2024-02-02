bc34b999a5aa79a7e795dfaa527f4c688a73185f
Nathan <1095903468@qq.com>
2024-02-03 01:09:01 +0800

    update

---
+++ DSU.cpp
@@ -0,0 +1,31 @@
+struct DSU{
+    vector<int> fa,sz;
+    DSU(){}
+    DSU(int n){
+        init(n);
+    }
+    void init(int n){
+        fa.resize(n+5);
+        iota(f.begin(),f.end(),0);
+        sz.assign(n,1);
+    }
+    int find(int x){
+        while(x!=fa[x]){
+            x=fa[x]=fa[fa[x]];
+        }
+        return x;
+    }
+    bool same(int a,int b){
+        return find(a)==find(b);
+    }
+    void merge(int a,int b){
+        a=find(a),b=find(b);
+        if (a==b) return ;
+        if (sz[a]<sz[b]) swap(a,b);
+        sz[a]+=sz[b];
+        fa[b]=a;
+    }
+    int size(int x){
+        return sz[find(x)];
+    }
+}
