6bc878b65ecaa39a2fae33278078b32c2ccbc603
NathanL1 <1095903468@qq.com>
2024-04-06 18:28:33 +0800

    update

--- KMP.cpp
+++ KMP.cpp
@@ -1,26 +1,17 @@
-#include <cstdio>
-#include <cstring>
-#define MAXN 1000005
-using namespace std;
-int n,m,kmp[MAXN];
-char s[MAXN],t[MAXN];
-int main(){
-	scanf("%s",s+1);
-	scanf("%s",t+1);
-	n=strlen(s+1);
-	m=strlen(t+1);
-	for (int i=2,j=0;i<=m;i++){
-		while(j&&t[j+1]!=t[i]) j=kmp[j];
+void KMP(string &s,string &t,vector<int> &ans){
+	int n=s.size(),m=t.size();
+	vector<int> kmp(m+1,-1);
+	for (int i=1,j=-1;i<m;i++){
+		while(j>=0&&t[j+1]!=t[i]) j=kmp[j];
 		if (t[j+1]==t[i]) j++;
 		kmp[i]=j;
 	}
-	for (int i=0,j=0;i<=n;i++){
-		while(j&&s[i]!=t[j+1]) j=kmp[j];
-		if (s[i]==t[j+1]) j++;
-		if (j==m){
-			printf("%d\n",i-m+1);
+	for (int i=0,j=-1;i<n;i++){
+		while(j>=0&&t[j+1]!=s[i]) j=kmp[j];
+		if (t[j+1]==s[i]) j++;
+		if (j==m-1){
+			ans.push_back(i-m+1);
 			j=kmp[j];
 		}
 	}
-	for (int i=1;i<=m;i++) printf("%d ",kmp[i]);
 }
